77524158b95f6a19f3ffe2e8e2675bd6dd6a7401
Maksimka66 <maxfese@gmail.com>
2023-12-03 06:01:08 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -10,5 +10,5 @@ const valueOfTheColor = document.querySelector('.color');
 
 btn.addEventListener('click', event => {
   body.style.backgroundColor = getRandomHexColor();
-  valueOfTheColor.textContent = getRandomHexColor();
+  valueOfTheColor.textContent = body.style.backgroundColor;
 });
